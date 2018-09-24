2b5974345081e0b55a3aa35c1f65d1e5ed133565
Greg Sherwood <gsherwood@squiz.net>
2018-09-24 12:44:51 +1000

    Bootstrapping projects is now dynamic

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -6,10 +6,34 @@ class Bootstrap {
 
     public static function load($project)
     {
-        // TODO: Go through project folder and load dynamically.
-        StorageFactory::createDataStore('comments');
-        StorageFactory::createDataRecordProperty('comment-content', 'text');
-        StorageFactory::createDataRecordProperty('threadid', 'unique');
+        $projectDir = dirname(dirname(dirname(dirname(__DIR__)))).'/Projects/'.$project;
+
+        // Add data stores.
+        $files = scandir($projectDir.'/Stores/Data');
+        foreach ($files as $file) {
+            if ($file[0] === '.'
+                || substr($file, -5) !== '.json'
+            ) {
+                continue;
+            }
+
+            $storeName = strtolower(substr($file, 0, -5));
+            StorageFactory::createDataStore($storeName);
+        }
+
+        // Add data record properties.
+        $files = scandir($projectDir.'/Properties/DataRecord');
+        foreach ($files as $file) {
+            if ($file[0] === '.'
+                || substr($file, -5) !== '.json'
+            ) {
+                continue;
+            }
+
+            $propName = strtolower(substr($file, 0, -5));
+            $propInfo = json_decode(file_get_contents($projectDir.'/Properties/DataRecord/'.$file), true);
+            StorageFactory::createDataRecordProperty($propName, $propInfo['type']);
+        }
 
     }//end load()
 
