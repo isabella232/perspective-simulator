e7466d0d47a3f5c425627a1c1364cfa9d0d2c552
Peter Nolland <pnolland@squiz.net>
2018-11-19 11:18:32 +1100

    Adds phpunit and base class for Simulator tests to extend from

---
+++ src/Tests/SimulatorTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Simulator Test class for Perspective Simulator.
+ *
+ * @package    Perspective
+ * @subpackage Simulator
+ * @author     Squiz Pty Ltd <products@squiz.net>
+ * @copyright  2018 Squiz Pty Ltd (ABN 77 084 670 600)
+ */
+
+namespace PerspectiveSimulator\Tests;
+
+use \PerspectiveSimulator\Bootstrap;
+
+/**
+ * SimulatorTest class
+ */
+class SimulatorTest extends \PHPUnit\Framework\TestCase
+{
+
+
+    /**
+     * Runs before the first test of the test case class is run.
+     *
+     * @return void
+     */
+    public static function setUpBeforeClass()
+    {
+        $calledClass = get_called_class();
+        $classParts  = explode('\\', $calledClass);
+
+        Bootstrap::disableRead();
+        Bootstrap::disableWrite();
+        Bootstrap::load($classParts[0]);
+
+    }//end setUpBeforeClass()
+
+
+    /**
+     * Gets the secret key for the project we are running tests on.
+     *
+     * @return string
+     */
+    public static function getSecretKey()
+    {
+        return \PerspectiveSimulator\Authentication::getSecretKey();
+
+    }
+
+
+}//end class
