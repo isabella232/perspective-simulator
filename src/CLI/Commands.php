4fb29470a3989de6253a3e82b0e820d4c177bb38
Peter Nolland <pnolland@squiz.net>
2019-01-23 09:06:56 +1100

    Simulator CLI commands are now dynamically registered

--- src/CLI/Commands.php
+++ src/CLI/Commands.php
@@ -10,38 +10,54 @@
 
 include_once $proot.'/vendor/autoload.php';
 if (isset($runner) === true && empty($runner) === false) {
-    $commands = [
-        '\\PerspectiveSimulator\\CLI\\Command\\API\\AddCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\API\\DeleteCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\API\\UpdateCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\APP\\AddCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\APP\\DeleteCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\CDN\\AddCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\CDN\\DeleteCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\CustomTypes\\AddCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\CustomTypes\\DeleteCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Deployment\\DeployCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Deployment\\DiffCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Deployment\\PreFlightCheckCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Gateway\\AddCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Project\\AddCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Project\\DeleteCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Property\\AddCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Property\\DeleteCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Property\\RenameCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Queue\\AddCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Queue\\DeleteCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Queue\\RenameCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Simulator\\CleanCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Simulator\\InstallCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Simulator\\ServerCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Simulator\\UpdateCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Stores\\AddCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Stores\\AddReferenceCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Stores\\DeleteCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Stores\\DeleteReferenceCommand',
-        '\\PerspectiveSimulator\\CLI\\Command\\Stores\\RenameCommand',
-    ];
+    // $commands = [
+    //     '\\PerspectiveSimulator\\CLI\\Command\\API\\AddCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\API\\DeleteCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\API\\UpdateCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\APP\\AddCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\APP\\DeleteCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\CDN\\AddCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\CDN\\DeleteCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\CustomTypes\\AddCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\CustomTypes\\DeleteCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Deployment\\DeployCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Deployment\\DiffCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Deployment\\PreFlightCheckCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Gateway\\AddCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Project\\AddCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Project\\DeleteCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Property\\AddCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Property\\DeleteCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Property\\RenameCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Queue\\AddCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Queue\\DeleteCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Queue\\RenameCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Simulator\\CleanCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Simulator\\InstallCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Simulator\\ServerCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Simulator\\UpdateCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Stores\\AddCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Stores\\AddReferenceCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Stores\\DeleteCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Stores\\DeleteReferenceCommand',
+    //     '\\PerspectiveSimulator\\CLI\\Command\\Stores\\RenameCommand',
+    // ];
+
+    $commands = [];
+    $dirs     = glob(dirname(__FILE__).'/Command/*', GLOB_ONLYDIR);
+    foreach ($dirs as $dir) {
+        $group         = basename($dir);
+        $groupCommands = scandir($dir);
+        foreach ($groupCommands as $command) {
+            if ($command[0] === '.'
+                || substr($command, -4) !== '.php'
+            ) {
+                continue;
+            }
+
+            $commands[] = '\\PerspectiveSimulator\\CLI\\Command\\'.$group.'\\'.str_replace('.php', '', $command);
+        }
+    }
 
     $runner->registerCommands($commands);
 }
