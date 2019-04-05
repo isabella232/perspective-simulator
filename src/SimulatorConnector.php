80ef652b4bbdf662ac785a04ee46cffa4b752393
Mohamed Haidar <mhaidar@squiz.net>
2019-04-05 14:59:06 +1100

    Implement send header function

--- src/SimulatorConnector.php
+++ src/SimulatorConnector.php
@@ -993,4 +993,23 @@ class SimulatorConnector implements \PerspectiveAPI\ConnectorInterface
     }//end moveDataRecord()
 
 
+    /**
+     * Use when you need to send headers.
+     *
+     * @param string  $header           The header string to send.
+     * @param boolean $replace          The optional replace parameter indicates whether the header should replace a
+     *                                  previous similar header, or add a second header of the same type.  By default it
+     *                                  will replace, but if you pass in FALSE as the second argument you can force
+     *                                  multiple headers of the same type.
+     * @param integer $httpResponseCode Forces the HTTP response code to the specified value.
+     *
+     * @return void
+     */
+    public static function sendHeader(string $header, bool $replace=true, int $httpResponseCode=null)
+    {
+        header($header, $replace, $httpResponseCode);
+
+    }//end sendHeader()
+
+
 }//end class
